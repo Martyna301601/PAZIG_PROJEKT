508b8cec544643bd78513c6f95d0e03ae5d94f1d
Martyna301601 <md301601@student.polsl.pl>
2024-03-22 13:24:10 +0100

    Update Wstępne wykrycie daltonizmu.py
    dodanie pustego okan

--- Wstępne wykrycie daltonizmu.py
+++ Wstępne wykrycie daltonizmu.py
@@ -1,3 +1,18 @@
-###
+import tkinter as tk
+from tkinter import *
+from PIL import ImageTk, Image
 
-##cokolwiek
+class LoginForn: 
+    def __init__(self, window):
+        self.window = window
+        self.window.geometry('1166x718')
+        self.window.state('zoomed')
+        self.window.resizable(0, 0)
+
+def page():
+    window = tk.Tk()
+    LoginForn(window)
+    window.mainloop()
+
+if __name__ == '__main__':
+    page()
