8eb37df57edaee5080b34b51e1243c481cadcaeb
Martyna301601 <md301601@student.polsl.pl>
2024-03-27 10:49:45 +0100

    Dodałam zasady

--- Wstępne wykrycie daltonizmu.py
+++ Wstępne wykrycie daltonizmu.py
@@ -53,6 +53,46 @@ class LoginForn:
         self.username_line = Canvas(self.login_frame, width=300, height=2.0, bg='#bdb9b1', highlightthickness=0)
         self.username_line.place(x=550, y=359)
 
+        ########IKONA PRZY LOGOWANIU########
+        self.username_icon = Image.open('mail.jpg')
+        self.username_icon = self.username_icon.resize((30, 30))
+        photo = ImageTk.PhotoImage(self.username_icon)
+        self.username_icon_label = Label(self.login_frame, image=photo, bg='#040405')
+        self.username_icon_label.image = photo
+        self.username_icon_label.place(x=550, y=325)
+
+        self.next_button = Button(self.login_frame, text='Dalej', bg='#6b6a69', fg='#bdb9b1', font=('yu gothic ui', 12, 'bold'), relief=FLAT, command=self.rules_page)
+        self.next_button.place(x=620, y=400, width=140, height=40)
+
+    ########ZASADY########
+    def rules_page(self):
+
+        self.login_frame.destroy()
+        self.next_frame = Frame(self.window, bg='#bdb9b1', width='950', height=600)
+        self.next_frame.place(x=200, y=70)
+        label_rules = Label(self.next_frame, text='Przeczytaj zasady:', font=('yu gothic ui', 13, 'bold'), bg='#bdb9b1', fg='black')
+        label_rules.place(x=400, y=20)
+
+        rules_text = """
+        
+        1. Wyświetlone zostaną zdjęcia, pod którymi wpisz liczbę widoczną na zdjęciu i naciśnij przycisk dalej lub wciśnij "ENTER".
+        
+        2. W przypadku problemu z rozpoznaniem liczb, naciśnij przycisk dalej lub wciśnij "ENTER".
+        
+        3. Wynik zostanie wyświetlony po zakończonym teście.
+        
+        4. Po zakończeniu możesz zapisać swój wynik do bazy.
+
+        5. Po zakończeniu klikając przycisk "wyślij na maila" otrzymasz wynik na podanego wcześniej maila.
+        
+        6. Kliknij start, aby rozpocząć test.
+        """
+
+        rules_label = Label(self.next_frame, text=rules_text, font=('yu gothic ui', 12), bg='#bdb9b1', fg='black', justify=LEFT)
+        rules_label.place(x=20, y=50)
+
+        self.start_button = Button(self.next_frame, text='Start', bg='#6b6a69', fg='#bdb9b1', font=('yu gothic ui', 12, 'bold'), relief=FLAT, command=self.start_test)
+        self.start_button.place(x=410, y=450, width=140, height=40)
 
 def page():
     window = tk.Tk()
